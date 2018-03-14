0d61a30c10bad4bcac36bd17b4ecb61e7251972d
CC <ltee12@hotmail.com>
2018-03-14 22:42:52 +0800

    测试module 只有library才能通过jitpack发布结果

--- storagetest/src/main/java/org/kreal/storagetest/MainActivity.kt
+++ storagetest/src/main/java/org/kreal/storagetest/MainActivity.kt
@@ -1,27 +1,32 @@
-package org.kreal.storage
+package org.kreal.storagetest
 
 import android.content.Intent
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import android.util.Log
+import org.kreal.storage.Storage
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var storage: Storage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val st = Storage(this)
-        val devs = st.devs
-        val documentFile = st.getDocumentFile("/${devs[0]}/Download") ?: return
+        storage = Storage(baseContext)
+        val devs = storage.devs
+        if (devs.size > 1)
+            if (!storage.isGrant(devs[1])) {
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+                startActivityForResult(intent, 22)
+            }
+        val documentFile = storage.getDocumentFile("/${devs[1]}/Download") ?: return
         Log.i("asd", documentFile.name)
     }
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
         contentResolver.takePersistableUriPermission(data.data, Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        storage.reload()
     }
-
 }
